7df5ebef166241ef434389c8f80c3d5eb4e0362e
sonluos <sonluos1013@gmail.com>
2026-05-16 20:50:17 +0900

    style: Research page matches Intro tone - white bg, gradient title, clean layout

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -1,5 +1,3 @@
-import { researchData } from '../data/research';
-import { ResearchCard } from '../components/ResearchCard';
 import './Research.css';
 
 export default function Research() {
@@ -14,23 +12,6 @@ export default function Research() {
       </section>
 
       <section className="research__body container">
-        <h2 className="research__subsection-heading">Publications &amp; Presentations</h2>
-        <div className="research__list" data-testid="publications-list">
-          {researchData.publications.map((entry) => (
-            <ResearchCard key={entry.id} item={entry} />
-          ))}
-        </div>
-
-        <h2 className="research__subsection-heading">MIR Recommendation System</h2>
-        <h2 className="research__subsection-heading">MIR with TDA</h2>
-        <h2 className="research__subsection-heading">Bone Suppression &amp; Anisotropic Diffusion</h2>
-        <h2 className="research__subsection-heading">Diffusion Education Program</h2>
-
-        <div className="research__list research__list--grid" data-testid="projects-list">
-          {researchData.projects.map((project) => (
-            <ResearchCard key={project.id} item={project} />
-          ))}
-        </div>
       </section>
     </main>
   );
